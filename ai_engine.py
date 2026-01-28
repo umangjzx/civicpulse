8112f48b477cf562443528433b468df877e5f741
Umang Jaiswal <159250278+umangjzx@users.noreply.github.com>
2026-01-29 02:00:29 +0530

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -4,3 +4,8 @@ if os.environ.get("VERCEL") != "1":
     import numpy as np
     import pandas as pd
     from sklearn.feature_extraction.text import TfidfVectorizer
+def predict_category(self, title, description):
+    return {
+        "category": "Roads",
+        "confidence": 0.91
+    }
